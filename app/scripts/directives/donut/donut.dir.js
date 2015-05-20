91990f991e4680b32a5a74d05e382c9af967da15
Steven Guichard <ssguichard@gmail.com>
2015-05-19 20:20:17 -0700

    Create svg even if no data - prevents weird resizing

--- app/scripts/directives/donut/donut.dir.js
+++ app/scripts/directives/donut/donut.dir.js
@@ -14,40 +14,45 @@ angular.module('policyEngine')
       },
       restrict: 'EA',
       link: function postLink(scope, element, attrs) {
+        var svg;
+
+        var radius = 108,
+          margin = {
+            top: 100,
+            bottom: 100,
+            left: 150,
+            right: 150
+          };
+
+        var width = radius * 2 + margin.left + margin.right;
+        var height = radius * 2 + margin.top + margin.bottom;
+
+        var labelBuffer = 60;
+        var circleWidth = 10;
+        var arc = d3.svg.arc()
+          .outerRadius(radius)
+          .innerRadius(radius - circleWidth);
+
+        var pie = d3.layout.pie()
+          .sort(null)
+          .value(function () {
+            return 1;
+          });
 
         var init = function () {
+
           d3.select(element[0]).selectAll('svg').remove();
 
-          var radius = 108,
-            margin = {
-              top: 100,
-              bottom: 100,
-              left: 150,
-              right: 150
-            };
 
-          var width = radius * 2 + margin.left + margin.right;
-          var height = radius * 2 + margin.top + margin.bottom;
-
-          var labelBuffer = 60;
-          var circleWidth = 10;
-          var arc = d3.svg.arc()
-            .outerRadius(radius)
-            .innerRadius(radius - circleWidth);
-
-          var pie = d3.layout.pie()
-            .sort(null)
-            .value(function () {
-              return 1;
-            });
-
-
-          var svg = d3.select(element.find('.donut')[0]).append("svg")
+          t svg = d3.select(element.find('.donut')[0]).append("svg")
             .attr("width", width)
             .attr("height", height)
             .append("g")
             .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
+        };
+
+        var drawArcs = function () {
 
           var g = svg.selectAll(".arc")
             .data(pie(scope.data))
@@ -87,11 +92,12 @@ angular.module('policyEngine')
             });
         };
 
+        init();
 
         scope.$watch('data', function () {
           if (scope.data && scope.data.length) {
-            console.log('data', scope.data);
             init();
+            drawArcs();
           }
         }, true);
       }
