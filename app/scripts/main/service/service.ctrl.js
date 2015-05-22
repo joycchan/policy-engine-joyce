9947a3ab37e3a900d09460e3c61554cd78407e86
Steven Guichard <ssguichard@gmail.com>
2015-05-21 17:18:56 -0700

    Fill in name automatically

--- app/scripts/main/service/service.ctrl.js
+++ app/scripts/main/service/service.ctrl.js
@@ -23,15 +23,18 @@ angular.module('policyEngine').controller('ServiceCtrl',
             {name: "TrustSEC Access", classifiers: "TrustSEC SGACL", custom: "Default"},
             {name: "Overlay TEP", classifiers: "Overlay-TEP-Type-HWTEP", custom: "Default"},
             {name: "Overlay Encap", classifiers: "Overlay-Encap-Type-VXLAN", custom: "Default"},
-            {name: "HTTP Traffic", classifiers: "TCP-80 Default", custom: "Default"},
+            {name: "HTTP Access", classifiers: "TCP-80 Default", custom: "Default"},
         ];
 
         $scope.existingContractSelection = function(selectedContract){
             $scope.service.ruleSet = selectedContract;
+            $scope.service.name += ':' + selectedContract.name;
             $state.go('main.service.meta');
         };
         $scope.existingGroupSelection = function(selectedGroup){
             $scope.service.group = selectedGroup;
+            $scope.service.name = selectedGroup.name;
+            $scope.service.name = selectedGroup.name;
             $state.go('main.service.contract.choose');
         };
 
