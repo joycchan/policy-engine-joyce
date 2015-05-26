cbee2a59b5e3621605625d22626f6425442d4227
Steven Guichard <ssguichard@gmail.com>
2015-05-26 16:53:48 -0700

    Display groups a service is assigned to in the sliding box

--- app/scripts/main/main.ctrl.js
+++ app/scripts/main/main.ctrl.js
@@ -3,7 +3,7 @@
 angular.module('policyEngine').controller('MainCtrl',
   function ($scope, $http, $state) {
 
-    $scope.serviceState = function() {
+    $scope.serviceState = function () {
       return $state.includes('main.service');
     };
 
@@ -11,7 +11,6 @@ angular.module('policyEngine').controller('MainCtrl',
 
     $scope.groups = [];
 
-    //$scope.allocations = [{"id":"1","type":"consume","item":{"id":"1","name":"All Employees","origin":"ISE (Active Directory)","provided":[],"consumed":[],"$$hashKey":"object:9"},"collection":[{"name":"SQL External Access","group":{"name":"Database Group"},"ruleSet":{"name":"Canned Contract"},"$$hashKey":"object:21"},{"name":"SQL Basic Access","group":{"name":"Database Group"},"ruleSet":{"name":"Canned Contract"},"$$hashKey":"object:19"},{"name":"SQL VIP Access","group":{"name":"Database Group"},"ruleSet":{"name":"Canned Contract"},"$$hashKey":"object:20"}],"$$hashKey":"object:29"}];
     $scope.allocations = [];
 
     var allocationId = $scope.allocations.length;
@@ -28,11 +27,49 @@ angular.module('policyEngine').controller('MainCtrl',
       return allocation;
     };
 
+    var allocationsByType = function (type) {
+      return _.filter($scope.allocations, function (allocation) {
+        return allocation.type === type;
+      });
+    };
+
+    var groupCentric = function () {
+      return allocationsByType('consume');
+    };
+
+    var serviceCentric = function () {
+      return allocationsByType('provide');
+    };
+
+    $scope.serviceConsumers = function (service) {
+      var groups = [];
+
+      var groupCentrics = _.filter(groupCentric(), function (allocation) {
+        return _.find(allocation.collection, function (s) {
+          return s.name === service.name;
+        })
+      });
+
+      _.each(groupCentrics, function(allocation) {
+        groups.push(allocation.item);
+      });
+
+      var serviceCentrics = _.filter(serviceCentric(), function(allocation) {
+        return allocation.item.name === service.name;
+      });
+
+      _.each(serviceCentrics, function(allocation) {
+        groups = groups.concat(allocation.collection);
+      });
+
+      return groups;
+    };
+
     $http.get('api/groups').success(function (data) {
       $scope.groups = data;
     });
 
-    $http.get('api/services').success(function(data) {
+    $http.get('api/services').success(function (data) {
       $scope.services = data;
     });
   }
