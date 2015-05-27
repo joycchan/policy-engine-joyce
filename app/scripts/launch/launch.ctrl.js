a7a2ca69270a552e921bfa48ee31ef770fe1d699
Thuongvu Ho <thuongvuho@gmail.com>
2015-05-27 15:46:30 -0700

    add: launch mockdata to json

--- app/scripts/launch/launch.ctrl.js
+++ app/scripts/launch/launch.ctrl.js
@@ -1,75 +1,19 @@
 'use strict';
 
 angular.module('policyEngine').controller('LaunchCtrl',
-  function($scope) {
+  function($scope, $http) {
 
-      $scope.mockSummaryData = {
-      'Service': [{
-          'name': 'Health',
-          'stat': 87,
-          'isGoodStat': true,
-          'drawerStats': [
-            {'name': 'Last Month','stat': 8,isGoodStat: true},
-            {'name': 'Past Quarter','stat': 2,isGoodStat: false}
-          ]
-        },{
-          'name': 'Count',
-          'stat': 8,
-          'isGoodStat': false,
-          'drawerStats': [
-            {'name': 'Last Month','stat': 8,isGoodStat: true},
-            {'name': 'Past Quarter','stat': 2,isGoodStat: false}
-          ]
-        },{
-          'name': 'Connections',
-          'stat': 16,
-          'isGoodStat': false,
-          'drawerStats': [
-            {'name': 'Last Month','stat': 8,isGoodStat: true},
-            {'name': 'Past Quarter','stat': 2,isGoodStat: false}
-          ]
-      }],
-      'Requests': [{
-          'name': 'New',
-          'stat': 5,
-          'isGoodStat': true,
-          'drawerStats': [
-            {'name': 'Last Month','stat': 8,isGoodStat: true},
-            {'name': 'Past Quarter','stat': 2,isGoodStat: false}
-          ]
-        },{
-          'name': 'All Pending',
-          'stat': 7,
-          'isGoodStat': false,
-          'drawerStats': [
-            {'name': 'Last Month','stat': 8,isGoodStat: true},
-            {'name': 'Past Quarter','stat': 2,isGoodStat: false}
-          ]
-      }],
-      'Groups': [{
-          'name': 'New',
-          'stat': 2,
-          'isGoodStat': true,
-          'drawerStats': [
-            {'name': 'Last Month','stat': 8,isGoodStat: true},
-            {'name': 'Past Quarter','stat': 2,isGoodStat: false}
-          ]
-          },{
-          'name': 'Count',
-          'stat': 9,
-          'isGoodStat': false,
-          'drawerStats': [
-            {'name': 'Last Month','stat': 8,isGoodStat: true},
-            {'name': 'Past Quarter','stat': 2,isGoodStat: false}
-          ]
-      }]
-      };
+      $scope.mockSummaryData = {};
+
+      $http.get('api/launch').success(function (data) {
+        $scope.mockSummaryData = data;
+      });
 
       $scope.launchCategories = [
         {'category': 'Service', img: 'images/icon_services_32.png'},
         {'category': 'Requests', img: 'images/icon_requests_32.png'},
         {'category': 'Groups', img: 'images/icon_groups_32.png'}
-      ]
+      ];
 
   }
 );
