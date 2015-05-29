1481504f3a05271311f2989af405426f0ee35ac1
Steven Guichard <ssguichard@gmail.com>
2015-05-29 12:46:33 -0700

    Added ruleSet service

--- app/scripts/main/service/service.ctrl.js
+++ app/scripts/main/service/service.ctrl.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('policyEngine').controller('ServiceCtrl',
-  function ($scope, $state, $modal) {
+  function ($scope, $state, $modal, ruleSets) {
 
     $scope.newGroup = function () {
       var modalInstance = $modal.open({
@@ -92,19 +92,13 @@ angular.module('policyEngine').controller('ServiceCtrl',
       $state.go('main.service.contract.' + $scope.state.ruleSetChoice);
     };
 
-    $scope.contractObj = [
-      {name: "SQL Access", classifiers: "SQL-Port-1443", custom: "Default"},
-      {name: "TrustSEC Access", classifiers: "TrustSEC SGACL", custom: "Default"},
-      {name: "Overlay TEP", classifiers: "Overlay-TEP-Type-HWTEP", custom: "Default"},
-      {name: "Overlay Encap", classifiers: "Overlay-Encap-Type-VXLAN", custom: "Default"},
-      {name: "HTTP Access", classifiers: "TCP-80 Default", custom: "Default"},
-    ];
-
     var setDefaultMetaData = function () {
       var group = ($scope.service.group && $scope.service.group.name) ? $scope.service.group.name : '';
       var ruleSet = ($scope.service.ruleSet && $scope.service.ruleSet.name) ? $scope.service.ruleSet.name : '';
       $scope.service.name = group + ':' + ruleSet;
     };
+
+    $scope.ruleSets = ruleSets.list;
 
     $scope.selectRuleSet = function (selectedContract) {
       $scope.service.ruleSet = selectedContract;
