16000dc664ecb2cfeac944140f56d59e39243e46
Thuongvu Ho <thuongvuho@gmail.com>
2015-05-28 17:48:59 -0700

    add: rule sets table

--- app/scripts/main/ruleSets/list/ruleSetsList.ctrl.js
+++ app/scripts/main/ruleSets/list/ruleSetsList.ctrl.js
@@ -1,7 +1,13 @@
 'use strict';
 
 angular.module('policyEngine').controller('RuleSetsListCtrl',
-  function($scope) {
+  function($scope, $http) {
+
+    $scope.rulesList = [];
+
+    $http.get('api/ruleSets').success(function (data) {
+      $scope.rulesList = data;
+    });
 
   }
 );
