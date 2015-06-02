e43b97f5c4b723c5a99a726fa8f1b9f3b4f75ed9
Steven Guichard <ssguichard@gmail.com>
2015-06-02 13:32:24 -0700

    Standardized spelling, removed merge conflict code

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -34,7 +34,7 @@ angular.module("policyEngine", [
 
         .state("main.configuration", {
           url: "configuration/",
-          templateUrl: "scripts/main/configuration/setUp.html",
+          templateUrl: "scripts/main/configuration/setup.html",
           controller: "ConfigurationCtrl"
         })
 
@@ -50,49 +50,6 @@ angular.module("policyEngine", [
           templateUrl: "scripts/main/service/service.html",
           controller: "ServiceCtrl"
         })
-
-          .state("main.service.group", {
-            abstract: true,
-            url: "group/",
-            templateUrl: "scripts/main/service/group/group.html"
-          })
-            .state("main.service.group.choose", {
-              url: "choose/",
-              templateUrl: "scripts/main/service/group/choose.html"
-            })
-            .state("main.service.group.new", {
-              url: "new/",
-              templateUrl: "scripts/main/service/group/new.html"
-            })
-            .state("main.service.group.existing", {
-              url: "existing/",
-              templateUrl: "scripts/main/service/group/existing.html"
-            })
-          .state("main.service.contract", {
-            abstract: true,
-            url: "contract/",
-            templateUrl: "scripts/main/service/contract/contract.html"
-          })
-            .state("main.service.contract.choose", {
-              url: "choose/",
-              templateUrl: "scripts/main/service/contract/choose.html"
-            })
-            .state("main.service.contract.new", {
-              url: "new/",
-              templateUrl: "scripts/main/service/contract/new.html"
-            })
-              .state("main.service.contract.addRule", {
-                url: "addRule/",
-                templateUrl: "scripts/main/service/contract/addRule.html"
-              })
-            .state("main.service.contract.existing", {
-              url: "existing/",
-              templateUrl: "scripts/main/service/contract/existing.html"
-            })
-          .state("main.service.meta", {
-            url: "meta/",
-            templateUrl: "scripts/main/service/meta.html"
-          })
 
        .state("main.allocations", {
 
