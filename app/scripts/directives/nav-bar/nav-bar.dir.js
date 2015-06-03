3b5d3f339f637fbf454206605ae4b6d90dba6c46
Steven Guichard <ssguichard@gmail.com>
2015-06-02 19:12:54 -0700

    Refactored nav bar into separate directive, added selection state

---
+++ app/scripts/directives/nav-bar/nav-bar.dir.js
@@ -0,0 +1,27 @@
+'use strict';
+
+angular.module('policyEngine')
+  .directive('navBar', function ($state) {
+    return {
+      templateUrl: 'scripts/directives/nav-bar/nav-bar.html',
+      scope: {
+        menu: '='
+      },
+      restrict: 'E',
+
+      link: function postLink(scope, element, attrs) {
+        scope.iconClasses = function(group) {
+          var selected = _.any(group.sections, function(section) {
+            return $state.includes(section.state);
+          });
+          var classes = {
+            current: selected
+          };
+          classes[group.cssClass] = true;
+          return classes;
+        };
+
+        scope.search = '';
+      }
+    };
+  });
