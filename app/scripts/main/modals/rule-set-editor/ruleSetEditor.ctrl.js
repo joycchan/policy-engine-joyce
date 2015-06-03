2c2ae553ec12dc0149e9036d79b4cdbe7eaeb8a7
Thuongvu Ho <thuongvuho@gmail.com>
2015-06-03 14:15:00 -0700

    add: ability to toggle edit mode vs view mode in rule set editor for individual rules

--- app/scripts/main/modals/rule-set-editor/ruleSetEditor.ctrl.js
+++ app/scripts/main/modals/rule-set-editor/ruleSetEditor.ctrl.js
@@ -37,11 +37,11 @@ angular.module('policyEngine').controller('RuleSetEditorCtrl',
     console.log("drop success, data:", data);
     console.log("index", index);
     if (data.dataType === 'classifier') {
-      if (!isExisting($scope.ruleSets()[index].classifiers, data.name)) {
+      if (!isExisting($scope.ruleSets()[index].classifiers, data.name) && $scope.editModeHash[index]) {
         $scope.ruleSets()[index].classifiers.push(data.name);
       }
     } else {
-      if (!isExisting($scope.ruleSets()[index].actions, data.name)) {
+      if (!isExisting($scope.ruleSets()[index].actions, data.name) && $scope.editModeHash[index]) {
         $scope.ruleSets()[index].actions.push(data.name);
       }
     }
@@ -51,8 +51,22 @@ angular.module('policyEngine').controller('RuleSetEditorCtrl',
     return _.extend(data, {'dataType': dataType});
   };
 
-  // $scope.isEditableHash = {};
+  $scope.editModeHash = {};
 
+  $scope.toggleEditRuleset = function(index) {
+    $scope.editModeHash[index] = !$scope.editModeHash[index];
+  };
+
+  $scope.isInEditModeForAnyRule = function() {
+    return _.any($scope.editModeHash, function(index) {
+      return index === true;
+    });
+  };
+
+  // to do
+    // allow user to delete rule, upon hover of table row + click on X
+    // allow user to add new rule, upon hover of bottom of table + click
+    // allow user to delte individual classifiers and actions
 
 
 });
