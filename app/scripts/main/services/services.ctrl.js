cca7aa242d0c1afc06ca39f7b67eb97a9c4393b3
Steven Guichard <ssguichard@gmail.com>
2015-06-04 09:41:53 -0700

    Added service catalog styling

--- app/scripts/main/services/services.ctrl.js
+++ app/scripts/main/services/services.ctrl.js
@@ -1,13 +1,60 @@
 'use strict';
 
 angular.module('policyEngine').controller('ServicesCtrl',
-    function($scope, $state, assignments) {
+  function ($scope, $state, assignments) {
 
-      $scope.assignService = function(service)  {
-        var allocation = assignments.create('provide', service)
-        $state.go('main.allocation.existing.provide', { allocationId: allocation.id });
-      };
+    $scope.assignService = function (service) {
+      var allocation = assignments.create('provide', service)
+      $state.go('main.allocation.existing.provide', {allocationId: allocation.id});
+    };
 
+    $scope.categories = [
+      {
+        label: 'Backup and Storage'
+      },
+      {
+        label: 'Business & Productivity Tools'
+      },
+      {
+        label: 'Database'
+      },
+      {
+        label: 'Email'
+      },
+      {
+        label: 'Internet Security'
+      },
+      {
+        label: 'Software Updates'
+      },
+      {
+        label: 'Voice and Video'
+      },
+    ];
+
+    $scope.providerGroups = [
+      {
+        name: 'My New Group'
+      },
+      {
+        name: 'SQL Servers Internal'
+      },
+      {
+        name: 'SQL Servers External'
+      }
+    ];
+
+    $scope.ruleSets = [
+      {
+        name: 'MY HQ Rule Set'
+      },
+      {
+        name: 'MY Basic Rule Set'
+      },
+      {
+        name: 'SQL Rule Set'
+      }
+    ];
   }
 );
 
