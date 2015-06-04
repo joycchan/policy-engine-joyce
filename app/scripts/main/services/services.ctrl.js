d524ed8137e3871db9e83b8b331cf3afe3a533e6
Steven Guichard <ssguichard@gmail.com>
2015-06-04 12:09:03 -0700

    Added service catalog filtering functionality

--- app/scripts/main/services/services.ctrl.js
+++ app/scripts/main/services/services.ctrl.js
@@ -8,53 +8,41 @@ angular.module('policyEngine').controller('ServicesCtrl',
       $state.go('main.allocation.existing.provide', {allocationId: allocation.id});
     };
 
-    $scope.categories = [
-      {
-        label: 'Backup and Storage'
-      },
-      {
-        label: 'Business & Productivity Tools'
-      },
-      {
-        label: 'Database'
-      },
-      {
-        label: 'Email'
-      },
-      {
-        label: 'Internet Security'
-      },
-      {
-        label: 'Software Updates'
-      },
-      {
-        label: 'Voice and Video'
-      },
-    ];
+    $scope.filteredServices = [];
 
-    $scope.providerGroups = [
-      {
-        name: 'My New Group'
-      },
-      {
-        name: 'SQL Servers Internal'
-      },
-      {
-        name: 'SQL Servers External'
-      }
-    ];
+    $scope.filters = {};
 
-    $scope.ruleSets = [
-      {
-        name: 'MY HQ Rule Set'
-      },
-      {
-        name: 'MY Basic Rule Set'
-      },
-      {
-        name: 'SQL Rule Set'
-      }
-    ];
+    var filterServices = function() {
+      $scope.filteredServices = $scope.services;
+      _.each(['category', 'group', 'ruleSet'], function(type) {
+        if($scope.filters[type]) {
+          $scope.filteredServices = _.filter($scope.filteredServices, function(service) {
+            return service[type].name === $scope.filters[type].name;
+          });
+        }
+      });
+    };
+
+    $scope.addFilter = function(filter, value) {
+      $scope.filters[filter] = value;
+      filterServices();
+    };
+
+    $scope.removeFilter = function(filter) {
+      delete $scope.filters[filter];
+      filterServices();
+    };
+
+    $scope.categories = [];
+    $scope.providerGroups = [];
+    $scope.ruleSets = [];
+
+    $scope.$watch('services', function() {
+      $scope.providerGroups = _.uniq(_.map($scope.services, function(service) { return service.group }), 'name');
+      $scope.ruleSets = _.uniq(_.map($scope.services, function(service) { return service.ruleSet }), 'name');
+      $scope.categories = _.uniq(_.map($scope.services, function(service) { return service.category }), 'name');
+      $scope.filteredServices = $scope.services;
+    });
   }
 );
 
