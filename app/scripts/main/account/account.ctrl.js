35cbd43f2b79f906ad35a7668f25f710e1c1789d
Steven Guichard <ssguichard@gmail.com>
2015-06-04 17:22:39 -0700

    Integrate with APIC

--- app/scripts/main/account/account.ctrl.js
+++ app/scripts/main/account/account.ctrl.js
@@ -1,8 +1,16 @@
 'use strict';
 
 angular.module('policyEngine').controller('AccountCtrl',
-  function($scope, configuration) {
+  function ($scope, configuration) {
 
     $scope.account = configuration.account;
+
+    $scope.isOdl = function() {
+      return configuration.account.type === 'odl';
+    };
+
+    $scope.isApic = function() {
+      return configuration.account.type === 'apic';
+    };
   }
 );
