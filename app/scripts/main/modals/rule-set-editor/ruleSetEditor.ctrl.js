50948300a9e51c34f5d73b14ef832c9dac60a59c
Steven Guichard <ssguichard@gmail.com>
2015-06-05 16:22:13 -0700

    Display hover state when editing rules

--- app/scripts/main/modals/rule-set-editor/ruleSetEditor.ctrl.js
+++ app/scripts/main/modals/rule-set-editor/ruleSetEditor.ctrl.js
@@ -1,77 +1,81 @@
-angular.module('policyEngine').controller('RuleSetEditorCtrl', 
+angular.module('policyEngine').controller('RuleSetEditorCtrl',
   function ($scope, $modalInstance, ruleSets, Actions, Classifiers, $stateParams, selectedRuleset) {
 
-  $scope.selectedRuleset = selectedRuleset; // local from resolve
+    $scope.selectedRuleset = selectedRuleset; // local from resolve
 
-  $scope.ok = function () {
-    $modalInstance.close($scope.selected);
-  };
+    $scope.ok = function () {
+      $modalInstance.close($scope.selected);
+    };
 
-  $scope.cancel = function () {
-    $modalInstance.dismiss('cancel');
-  };
+    $scope.cancel = function () {
+      $modalInstance.dismiss('cancel');
+    };
 
-  $scope.existingClassifiers = Classifiers.list;
+    $scope.existingClassifiers = Classifiers.list;
 
-  $scope.existingActions = Actions.list;
+    $scope.existingActions = Actions.list;
 
-  $scope.deleteRuleset = function() {
+    $scope.deleteRuleset = function () {
 
-  };
+    };
 
-  $scope.onDragComplete = function(data,evt){
-    // console.log("drag success, data:", data);
-  }
+    $scope.onDragComplete = function (data, evt) {
+      // console.log("drag success, data:", data);
+    }
 
-  var isExistingRuleset = function(list, newItem) {
-    return _.any(list, function(item) {
-      return item === newItem;
-    });
-  }
+    var isExistingRuleset = function (list, newItem) {
+      return _.any(list, function (item) {
+        return item === newItem;
+      });
+    }
 
-  $scope.onDropComplete = function(data,index){
-    if (data.dataType === 'classifier') {
-      if (!isExistingRuleset($scope.selectedRuleset[index].classifiers, data.name) && $scope.editModeHash[index]) {
+    $scope.addClassifier = function (data, index) {
+      if (data.dataType === 'classifier'
+        && !isExistingRuleset($scope.selectedRuleset[index].classifiers, data.name)
+        && $scope.editModeHash[index]) {
         $scope.selectedRuleset[index].classifiers.push(data.name);
       }
-    } else {
-      if (!isExistingRuleset($scope.selectedRuleset[index].actions, data.name) && $scope.editModeHash[index]) {
+    };
+
+    $scope.addAction = function (data, index) {
+      if (data.dataType === 'action'
+        && !isExistingRuleset($scope.selectedRuleset[index].actions, data.name)
+        && $scope.editModeHash[index]) {
         $scope.selectedRuleset[index].actions.push(data.name);
       }
-    }
-  }
+    };
 
-  $scope.decorateData = function(data, dataType) {
-    return _.extend(data, {'dataType': dataType});
-  };
+    $scope.decorateData = function (data, dataType) {
+      return _.extend(data, {'dataType': dataType});
+    };
 
-  $scope.editModeHash = {
-     // setting state for dev purposes
-    0: true
-    // ,
-  };
+    $scope.editModeHash = {
+      // setting state for dev purposes
+      0: true
+      // ,
+    };
 
-  $scope.toggleEditRuleset = function(index) {
-    $scope.editModeHash[index] = !$scope.editModeHash[index];
-  };
+    $scope.toggleEditRuleset = function (index) {
+      $scope.editModeHash[index] = !$scope.editModeHash[index];
+    };
 
-  $scope.isInEditModeForAnyRule = function() {
-    return _.any($scope.editModeHash, function(index) {
-      return index === true;
-    });
-  };
+    $scope.isInEditModeForAnyRule = function () {
+      return _.any($scope.editModeHash, function (index) {
+        return index === true;
+      });
+    };
 
-  // to do
+    // to do
     // allow user to delete rule, upon hover of table row + click on X
     // allow user to add new rule, upon hover of bottom of table + click
     // allow user to delte individual classifiers and actions
 
-  $scope.ok = function () {
-    $modalInstance.close();
-  };
+    $scope.ok = function () {
+      $modalInstance.close();
+    };
 
-  $scope.cancel = function () {
-    $modalInstance.dismiss('cancel');
-  };
+    $scope.cancel = function () {
+      $modalInstance.dismiss('cancel');
+    };
 
-});
+  });
