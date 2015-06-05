29e41bc3b4aeb7becc498c4dcf82136dde630b40
Steven Guichard <ssguichard@gmail.com>
2015-06-05 12:27:45 -0700

    Fixed button positioning

--- app/scripts/main/configuration/configuration.ctrl.js
+++ app/scripts/main/configuration/configuration.ctrl.js
@@ -2,9 +2,6 @@
 
 angular.module('policyEngine').controller('ConfigurationCtrl',
     function ($scope) {
-       $scope.config = {
-           portNo : 123,
-           port : 123
-           };
+       $scope.config = {};
     }
 );
