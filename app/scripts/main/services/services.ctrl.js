2257e038afbbf667eacd2475fee427ea834e13cc
Steven Guichard <ssguichard@gmail.com>
2015-06-05 20:18:23 -0700

    Fixed serviceConsumers

--- app/scripts/main/services/services.ctrl.js
+++ app/scripts/main/services/services.ctrl.js
@@ -8,27 +8,33 @@ angular.module('policyEngine').controller('ServicesCtrl',
       $state.go('main.allocation.existing.provide', {allocationId: allocation.id});
     };
 
+    $scope.serviceConsumers = assignments.serviceConsumers;
+
+    $scope.serviceConsumersString = function (service) {
+      return _.pluck($scope.serviceConsumers(service), 'name').join(', ');
+    };
+
     $scope.filteredServices = [];
 
     $scope.filters = {};
 
-    var filterServices = function() {
+    var filterServices = function () {
       $scope.filteredServices = $scope.services;
-      _.each(['category', 'group', 'ruleSet'], function(type) {
-        if($scope.filters[type]) {
-          $scope.filteredServices = _.filter($scope.filteredServices, function(service) {
+      _.each(['category', 'group', 'ruleSet'], function (type) {
+        if ($scope.filters[type]) {
+          $scope.filteredServices = _.filter($scope.filteredServices, function (service) {
             return service[type].name === $scope.filters[type].name;
           });
         }
       });
     };
 
-    $scope.addFilter = function(filter, value) {
+    $scope.addFilter = function (filter, value) {
       $scope.filters[filter] = value;
       filterServices();
     };
 
-    $scope.removeFilter = function(filter) {
+    $scope.removeFilter = function (filter) {
       delete $scope.filters[filter];
       filterServices();
     };
@@ -37,17 +43,23 @@ angular.module('policyEngine').controller('ServicesCtrl',
     $scope.providerGroups = [];
     $scope.ruleSets = [];
 
-    $scope.$watch('services', function() {
-      $scope.providerGroups = _.uniq(_.map($scope.services, function(service) { return service.group }), 'name');
-      $scope.ruleSets = _.uniq(_.map($scope.services, function(service) { return service.ruleSet }), 'name');
-      $scope.categories = _.uniq(_.map($scope.services, function(service) { return service.category }), 'name');
+    $scope.$watch('services', function () {
+      $scope.providerGroups = _.uniq(_.map($scope.services, function (service) {
+        return service.group
+      }), 'name');
+      $scope.ruleSets = _.uniq(_.map($scope.services, function (service) {
+        return service.ruleSet
+      }), 'name');
+      $scope.categories = _.uniq(_.map($scope.services, function (service) {
+        return service.category
+      }), 'name');
       $scope.filteredServices = $scope.services;
     });
 
 
     $scope.selectedCheckBoxes = {};
 
-    $scope.isRowInListSelected = function(id) {
+    $scope.isRowInListSelected = function (id) {
       return $scope.selectedCheckBoxes[id] === true;
     }
 
