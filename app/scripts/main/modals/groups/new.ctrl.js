b1c3909d65018ac57d0e1f9909eabf1694cde589
Steven Guichard <ssguichard@gmail.com>
2015-06-07 21:34:16 -0700

    Added new pool selection interaction

--- app/scripts/main/modals/groups/new.ctrl.js
+++ app/scripts/main/modals/groups/new.ctrl.js
@@ -7,9 +7,37 @@ angular.module('policyEngine').controller('NewGroupCtrl', function ($scope, grou
   $scope.pools = [
     '10.0.35.1/24',
     '10.0.36.1/24',
-    'MAC pool#1',
-    'MAC pool#2'
+    '10.4.28.1/24',
+    '10.4.30.1/24',
+    '10.20.101.1/24',
+    '10.20.102.1/24',
+    '10.20.102.2/24',
+    '10.30.0.1/24',
+    '10.30.0.2/24',
+    '10.30.0.3/24'
   ];
+
+  $scope.selectedPools = [];
+
+  $scope.poolsExpanded = false;
+
+  $scope.removePool = function(pool) {
+    _.remove($scope.selectedPools, function(p) {
+      return p === pool;
+    });
+  };
+
+  $scope.poolSelected = function(pool) {
+    return _.includes($scope.selectedPools, pool);
+  };
+
+  $scope.togglePool = function(pool) {
+    if($scope.poolSelected(pool)) {
+      $scope.removePool(pool);
+    } else {
+      $scope.selectedPools.push(pool);
+    }
+  };
 
   $scope.ok = function () {
     $scope.service.group = groups.create($scope.group);
