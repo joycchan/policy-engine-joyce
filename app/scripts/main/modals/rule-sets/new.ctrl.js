7f2fe11866c02d2e06b16bf423ee8e02f7f38f41
Thuongvu Ho <thuongvuho@gmail.com>
2015-06-08 17:27:23 -0700

    add: move rule set generation logic to service, adjust parts of new rule sets modal to handle changes

--- app/scripts/main/modals/rule-sets/new.ctrl.js
+++ app/scripts/main/modals/rule-sets/new.ctrl.js
@@ -1,28 +1,20 @@
 angular.module('policyEngine').controller('NewRuleSetCtrl', function ($scope, $state, ruleSets, $modal, Modals) {
 
-  $scope.ruleSets = [
-    // put the ruleset in here
+  $scope.newRuleSets = [
+    ruleSets.generateEmptyRuleSet()
   ];
-  $scope.ruleSet = {
-    name: "New Rule Set",
-    classifiers: [],
-    actions: [],
-    custom: "Custom",
-    id: Math.floor(Math.random() * 10000)
-    // while the user is in main.ruleSetsEdit, the id allows the user to select a rule set out of the list to modify
-    // logic in that state depends on $stateParams because it is not a modal w/ one parent controller
-    // whereas in main.service, the data is on one controller which is the state of truth for the modals' data
-    // TODO: remove client-side id generation
-  };
 
   $scope.addRules = function () {
-    $modal.open(Modals.rulesetEditor([$scope.ruleSet])); // pass scope.ruleSets into here
-    // when the modal closes, set the modified array of data to $scope.ruleSets
-    // 
+    $modal.open(Modals.rulesetEditor($scope.newRuleSets)); // pass scope.ruleSets into here
+    
+    modalInstance.result.then(function () {
+      // when the modal closes, set the modified array of data to $scope.newRuleSets
+    }, function () {
+    });
   };
 
   $scope.disabled = function () {
-    return ($scope.ruleSet.classifiers.length === 0) || ($scope.ruleSet.actions.length === 0);
+    return ($scope.newRuleSets[0].classifiers.length === 0) || ($scope.newRuleSets[0].actions.length === 0);
   };
 
 
--- app/scripts/services/ruleSets.js
+++ app/scripts/services/ruleSets.js
@@ -31,15 +31,25 @@ angular.module('policyEngine').factory('ruleSets',
       });
     };
 
-    // service.generateEmptyRuleSet = function() {
-      
-    //}
+    service.generateEmptyRuleSet = function() {
+      return {
+        name: "New Rule Set",
+        classifiers: [],
+        actions: [],
+        custom: "Custom",
+        id: Math.floor(Math.random() * 10000)
+        // while the user is in main.ruleSetsEdit, the id allows the user to select a rule set out of the list to modify
+        // logic in that state depends on $stateParams because it is not a modal w/ one parent controller
+        // whereas in main.service, the data is on one controller which is the state of truth for the modals' data
+        // TODO: remove client-side id generation
+      };
+    };
 
     // service.edit = function(id, data) {
       // find item in list via id property
       // make a put request
       // on success, set the the new list to data returned from server
-    }
+    // }
 
     return service;
   });
