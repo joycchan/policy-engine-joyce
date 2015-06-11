d39f93a362b44287e53eaee3652b2fe24cdca8e1
Thuongvu Ho <thuongvuho@gmail.com>
2015-06-11 11:31:02 -0700

    chore: rename file

--- app/scripts/main/classifiers-library/edit/classfiersEdit.ctrl.js
+++
@@ -1,8 +0,0 @@
-angular.module('policyEngine').controller('ClassifiersEditCtrl',
-  function($scope, $modal, $stateParams, Classifiers) {
-    $scope.navTabLinks = [{
-      'name': 'Settings',
-      'uiSref': 'main.classifiersLibraryEdit.settings'
-    }];
-  }
-);
