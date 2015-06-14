0075662110f58ceac6b80cec5ee8983938986b8d
Steven Guichard <ssguichard@gmail.com>
2015-06-14 16:27:06 -0700

    Display all classifiers in rule set list

--- app/scripts/filters/subRulesToString.js
+++ app/scripts/filters/subRulesToString.js
@@ -3,8 +3,9 @@
 angular.module('policyEngine').filter('subRulesToString', [
   function() {
     return function(rules, key) {
+      console.log('rules', rules);
       return _.map(rules, function(rule) {
-        return _.pluck(rule[key], 'name');
+        return _.pluck(rule[key], 'name').join(', ');
       }).join(', ');
     };
   }
