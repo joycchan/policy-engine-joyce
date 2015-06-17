cc11b909f925fb80f6a83f87b18ec1ce92c3e5b2
Thuongvu Ho <thuongvuho@gmail.com>
2015-06-17 11:39:04 -0700

    fix: services edit - rule set will now only update the model at the end of editing rule set

--- app/scripts/main/services/edit/rule-set/ruleSet.ctrl.js
+++ app/scripts/main/services/edit/rule-set/ruleSet.ctrl.js
@@ -3,10 +3,8 @@
 angular.module('policyEngine').controller('ServicesEditRuleSet',
   function ($scope, PolicyStore, PolicyActions, $modal, Modals) {
 
-    $scope.ruleSet = angular.copy(_.first(PolicyStore.RuleSets.where({'name': $scope.service().ruleSet.name})));
-
     $scope.editRule = function () {
-      var modalInstance = $modal.open(Modals.ruleSetEditor($scope.ruleSet));
+      var modalInstance = $modal.open(Modals.ruleSetEditor(angular.copy($scope.ruleSet)));
 
       modalInstance.result.then(function (updatedRuleSet) {
         PolicyActions.UpdateRuleSet(updatedRuleSet);
@@ -22,5 +20,12 @@ angular.module('policyEngine').controller('ServicesEditRuleSet',
       }, function () {
       });
     };
+
+    $scope.$watchGroup(['$routeChangeSuccess', function () {
+      return PolicyStore.RuleSets.all();
+    }], function () {
+      // TODO: update this to search via id rather than name property
+      $scope.ruleSet = angular.copy(_.first(PolicyStore.RuleSets.where({'name': $scope.service().ruleSet.name})));
+    });
 
   });
