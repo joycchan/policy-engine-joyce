a30212d3e1bb812d198ee9765072f8791a62ee19
Steven Guichard <ssguichard@gmail.com>
2015-06-22 16:37:40 -0700

    Removed unused code

--- app/scripts/main/modals/rule-set-editor/ruleSetEditor.ctrl.js
+++ app/scripts/main/modals/rule-set-editor/ruleSetEditor.ctrl.js
@@ -1,125 +1,109 @@
 angular.module('policyEngine').controller('RuleSetEditorCtrl',
+  function ($scope, $modalInstance, $stateParams, selectedRuleSet, PolicyStore) {
 
+    $scope.selectedRuleSet = selectedRuleSet; // local from resolve
 
-  function ($scope, $modalInstance, $stateParams, selectedRuleSet, PolicyStore, Modals, $modal) {
+    $scope.existingClassifiers = PolicyStore.Classifiers.all.bind(PolicyStore.Classifiers);
 
+    $scope.existingActions = PolicyStore.Actions.all.bind(PolicyStore.Actions);
 
-      $scope.selectedRuleSet = selectedRuleSet; // local from resolve
-
-      $scope.existingClassifiers = PolicyStore.Classifiers.all.bind(PolicyStore.Classifiers);
-
-      $scope.existingActions = PolicyStore.Actions.all.bind(PolicyStore.Actions);
-
-      $scope.ok = function () {
-        if ($scope.areAllRulesValid()) {
-          $modalInstance.close($scope.selectedRuleSet);
-        }
-      };
-
-      $scope.cancel = function () {
-        $modalInstance.dismiss('cancel');
-      };
-
-      var isExistingRuleSet = function (list, newItem) {
-        return _.any(list, function (item) {
-          return item.name === newItem;
-        });
+    $scope.ok = function () {
+      if ($scope.areAllRulesValid()) {
+        $modalInstance.close($scope.selectedRuleSet);
       }
+    };
 
-      $scope.addClassifier = function (metaObject, index) {
-        if (metaObject.type === 'classifier'
-          && !isExistingRuleSet($scope.selectedRuleSet.rules[index].classifiers, metaObject.data.name)
-          && $scope.editModeHash[index]) {
+    $scope.cancel = function () {
+      $modalInstance.dismiss('cancel');
+    };
 
-          $scope.selectedRuleSet.rules[index].classifiers.push({"name": metaObject.data.name});
-        }
-      };
+    var isExistingRuleSet = function (list, newItem) {
+      return _.any(list, function (item) {
+        return item.name === newItem;
+      });
+    }
 
-      $scope.addAction = function (metaObject, index) {
-        if (metaObject.type === 'action'
-          && !isExistingRuleSet($scope.selectedRuleSet.rules[index].actions, metaObject.data.name)
-          && $scope.editModeHash[index]) {
+    $scope.addClassifier = function (metaObject, index) {
+      if (metaObject.type === 'classifier'
+        && !isExistingRuleSet($scope.selectedRuleSet.rules[index].classifiers, metaObject.data.name)
+        && $scope.editModeHash[index]) {
 
-          $scope.selectedRuleSet.rules[index].actions.push({"name": metaObject.data.name});
-        }
-      };
-
-      $scope.editModeHash = {
-        // setting state for dev purposes
-        0: true
-        // ,
-      };
-
-      $scope.toggleEditRuleSet = function (index) {
-        $scope.editModeHash[index] = !$scope.editModeHash[index];
-      };
-
-      $scope.isInEditModeForAnyRule = function () {
-        return _.any($scope.editModeHash, function (index) {
-          return index === true;
-        });
-      };
-
-      $scope.search = {
-        classifiers: {name:''},
-        actions: {name:''}
-      };
-
-
-
-      $scope.customClassifiers = function () {
-        //$modal.open(Modals.customClassifier([$scope.rule]));
-      };
-
-      $scope.customActions = function () {
-        //$modal.open(Modals.customActions([$scope.rule]));
-      };
-
-
-      var generateEmptyRule = function() {
-        return {
-          classifiers: [],
-          actions: []
-        };
-      };
-
-
-      $scope.addRule = function() {
-        if ($scope.areAllRulesValid()) {
-          $scope.selectedRuleSet.rules.push(generateEmptyRule());
-          $scope.editModeHash[$scope.selectedRuleSet.rules.length - 1] = true;
-        }
-      };
-
-      $scope.areAllRulesValid = function() {
-        return _.all($scope.selectedRuleSet.rules, function(rule) {
-          return doesRuleIncludeActionAndClassifier(rule);
-        })
+        $scope.selectedRuleSet.rules[index].classifiers.push({"name": metaObject.data.name});
       }
+    };
 
-      var doesRuleIncludeActionAndClassifier = function(rule) {
-        return (rule.actions && rule.actions.length) && (rule.classifiers && rule.classifiers.length);
-      };
+    $scope.addAction = function (metaObject, index) {
+      if (metaObject.type === 'action'
+        && !isExistingRuleSet($scope.selectedRuleSet.rules[index].actions, metaObject.data.name)
+        && $scope.editModeHash[index]) {
 
-
-      // to do
-      // allow user to delete rule, upon hover of table row + click on X
-      // allow user to add new rule, upon hover of bottom of table + click
-      // allow user to delte individual classifiers and actions
-
-      $scope.deleteRule = function(index) {
-        $scope.selectedRuleSet.rules.splice(index, 1);
-      };
-
-      $scope.deleteClassifier = function(rulesIndex, classifier) {
-        _.remove($scope.selectedRuleSet.rules[rulesIndex].classifiers, function(c) {
-          return c.name === classifier.name;
-        });
-      };
-      $scope.deleteAction = function(rulesIndex, action) {
-        _.remove($scope.selectedRuleSet.rules[rulesIndex].actions, function(c) {
-          return c.name === action.name;
-        });
+        $scope.selectedRuleSet.rules[index].actions.push({"name": metaObject.data.name});
       }
+    };
+
+    $scope.editModeHash = {
+      // setting state for dev purposes
+      0: true
+      // ,
+    };
+
+    $scope.toggleEditRuleSet = function (index) {
+      $scope.editModeHash[index] = !$scope.editModeHash[index];
+    };
+
+    $scope.isInEditModeForAnyRule = function () {
+      return _.any($scope.editModeHash, function (index) {
+        return index === true;
+      });
+    };
+
+    $scope.search = {
+      classifiers: {name:''},
+      actions: {name:''}
+    };
+
+    var generateEmptyRule = function() {
+      return {
+        classifiers: [],
+        actions: []
+      };
+    };
+
+    $scope.addRule = function() {
+      if ($scope.areAllRulesValid()) {
+        $scope.selectedRuleSet.rules.push(generateEmptyRule());
+        $scope.editModeHash[$scope.selectedRuleSet.rules.length - 1] = true;
+      }
+    };
+
+    $scope.areAllRulesValid = function() {
+      return _.all($scope.selectedRuleSet.rules, function(rule) {
+        return doesRuleIncludeActionAndClassifier(rule);
+      })
+    }
+
+    var doesRuleIncludeActionAndClassifier = function(rule) {
+      return (rule.actions && rule.actions.length) && (rule.classifiers && rule.classifiers.length);
+    };
+
+    // to do
+    // allow user to delete rule, upon hover of table row + click on X
+    // allow user to add new rule, upon hover of bottom of table + click
+    // allow user to delte individual classifiers and actions
+
+    $scope.deleteRule = function(index) {
+      $scope.selectedRuleSet.rules.splice(index, 1);
+    };
+
+    $scope.deleteClassifier = function(rulesIndex, classifier) {
+      _.remove($scope.selectedRuleSet.rules[rulesIndex].classifiers, function(c) {
+        return c.name === classifier.name;
+      });
+    };
+    $scope.deleteAction = function(rulesIndex, action) {
+      _.remove($scope.selectedRuleSet.rules[rulesIndex].actions, function(c) {
+        return c.name === action.name;
+      });
+    }
 
   });
--- app/scripts/services/Modals.js
+++ app/scripts/services/Modals.js
@@ -1,85 +1,61 @@
 'use strict';
 
 angular.module('policyEngine').factory('Modals',
-    function () {
-      var service = {
-        newGroup: {
-          'animation': false,
-          templateUrl: 'scripts/main/modals/groups/new.html',
-          windowTemplateUrl: 'scripts/templates/modal/window.html',
-          controller: 'NewGroupCtrl',
-          size: 'lg'
-        },
-        'existingGroup': {
+  function () {
+    var service = {
+      newGroup: {
+        'animation': false,
+        templateUrl: 'scripts/main/modals/groups/new.html',
+        windowTemplateUrl: 'scripts/templates/modal/window.html',
+        controller: 'NewGroupCtrl',
+        size: 'lg'
+      },
+      'existingGroup': {
+        animation: false,
+        templateUrl: 'scripts/main/modals/groups/existing.html',
+        windowTemplateUrl: 'scripts/templates/modal/window.html',
+        controller: 'ExistingGroupCtrl',
+        size: 'lg'
+      },
+      'newRuleSet': {
+        animation: false,
+        templateUrl: 'scripts/main/modals/rule-sets/new.html',
+        windowTemplateUrl: 'scripts/templates/modal/window.html',
+        controller: 'NewRuleSetCtrl',
+        size: 'lg'
+      },
+      'existingRuleset': {
+        animation: false,
+        templateUrl: 'scripts/main/modals/rule-sets/existing.html',
+        windowTemplateUrl: 'scripts/templates/modal/window.html',
+        controller: 'ExistingRuleSetCtrl',
+        size: 'lg'
+      },
+      'ruleSetEditor': function(rulesetsList) {
+        return {
           animation: false,
-          templateUrl: 'scripts/main/modals/groups/existing.html',
+          templateUrl: 'scripts/main/modals/rule-set-editor/rule-set-editor.html',
+          controller: 'RuleSetEditorCtrl',
+          size: 'lg',
+          windowClass: 'ruleEditor-modal',
           windowTemplateUrl: 'scripts/templates/modal/window.html',
-          controller: 'ExistingGroupCtrl',
-          size: 'lg'
-        },
-        'newRuleSet': {
-          animation: false,
-          templateUrl: 'scripts/main/modals/rule-sets/new.html',
-          windowTemplateUrl: 'scripts/templates/modal/window.html',
-          controller: 'NewRuleSetCtrl',
-          size: 'lg'
-        },
-        'existingRuleset': {
-          animation: false,
-          templateUrl: 'scripts/main/modals/rule-sets/existing.html',
-          windowTemplateUrl: 'scripts/templates/modal/window.html',
-          controller: 'ExistingRuleSetCtrl',
-          size: 'lg'
-        },
-        'ruleSetEditor': function(rulesetsList) {
-          return {
-            animation: false,
-            templateUrl: 'scripts/main/modals/rule-set-editor/rule-set-editor.html',
-            controller: 'RuleSetEditorCtrl',
-            size: 'lg',
-            windowClass: 'ruleEditor-modal',
-            windowTemplateUrl: 'scripts/templates/modal/window.html',
-            resolve: {
-              selectedRuleSet: function() {
-                return rulesetsList;
-              }
+          resolve: {
+            selectedRuleSet: function() {
+              return rulesetsList;
             }
           }
-        },
-        'customClassifier': function(customClassifiers){
-          return {
-            animation: false,
-            templateUrl: 'scripts/main/modals/rule-set-editor/customClassifier/classifiers.html',
-            controller: 'customClassifierCtrl',
-            size: 'lg',
-            windowClass: 'addCustomerClassifier-modal',
-            windowTemplateUrl: 'scripts/templates/modal/window.html',
-
-          }
-
-        },
-        'customActions': function(customActions){
-          return {
-            animation: false,
-            templateUrl: 'scripts/main/modals/rule-set-editor/action/action.html',
-            controller: 'customActionsCtrl',
-            size: 'lg',
-            windowClass: 'addCustomerActions-modal',
-            windowTemplateUrl: 'scripts/templates/modal/window.html'
-
-          }
-
-        },
-        newAssignment: {
-          animation: false,
-          templateUrl: 'scripts/main/modals/new-assignment/new-assignment.html',
-          controller: 'NewAssignmentCtrl',
-          size: 'lg',
-          windowClass: 'new-assignment-modal',
-          windowTemplateUrl: 'scripts/templates/modal/window.html',
         }
-      };
+      },
+      newAssignment: {
+        animation: false,
+        templateUrl: 'scripts/main/modals/new-assignment/new-assignment.html',
+        controller: 'NewAssignmentCtrl',
+        size: 'lg',
+        windowClass: 'new-assignment-modal',
+        windowTemplateUrl: 'scripts/templates/modal/window.html',
+      }
+    };
 
-      return service;
-    });
+    return service;
+  });
 
