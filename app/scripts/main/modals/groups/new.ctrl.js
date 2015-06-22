b6f177a5b96d7ca4eb2e3c0babca746637dca350
shegoyal <shegoyal@cisco.com>
2015-06-22 12:18:10 -0700

    Conditions in New Group

--- app/scripts/main/modals/groups/new.ctrl.js
+++ app/scripts/main/modals/groups/new.ctrl.js
@@ -1,5 +1,5 @@
 angular.module('policyEngine').controller('NewGroupCtrl', function ($scope, PolicyActions) {
-
+ $scope.types = {};
   $scope.group = {
     name: "New Group",
     pools: []
@@ -9,4 +9,32 @@ angular.module('policyEngine').controller('NewGroupCtrl', function ($scope, Poli
     var group = PolicyActions.CreateGroup($scope.group);
     $scope.createGroup(group);
   };
+
+  $scope.posture = [
+  { name:'Device'},
+  { name:'Location'}
+  ];
+
+  $scope.device = [
+    { name:'Company Desktop'},
+    { name:'Company Phone'},
+    { name:'All Other Devices'}
+  ];
+
+  $scope.location = [
+      { name:'On-site'},
+      { name:'Off-site(VPN)'},
+      { name:'Everywhere else'}
+  ];
+
+  $scope.changeData = function() {
+          //console.log($scope.itemsuper);
+
+          if($scope.itemsuper.name == "Device") {
+              $scope.types = $scope.device;
+          } else if($scope.itemsuper.name == "Location") {
+              $scope.types = $scope.location;
+          }
+      };
+
 });
