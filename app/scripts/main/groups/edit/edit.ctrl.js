6044e3ffb7d0fc0fef3319c897cb564ab61673e2
Thuongvu Ho <thuongvuho@gmail.com>
2015-06-21 22:43:39 -0700

    add: groups detail / services provided + services consumed allow a user to navigate to service edit view + to assignment edit view

--- app/scripts/main/groups/edit/edit.ctrl.js
+++ app/scripts/main/groups/edit/edit.ctrl.js
@@ -24,6 +24,11 @@ angular.module('policyEngine').controller('GroupsEditCtrl',
       $scope.group = angular.copy(PolicyStore.Groups.find({id: $stateParams.groupId}));
     });
 
+    // Mock data for the Services Provided and Services Consumed tables
+    var _services = PolicyStore.Services.all.bind(PolicyStore.Services);
+    $scope.servicesProvided = _services().slice(0, 2);
+    $scope.servicesConsumed = _services().slice(2, 4);
+    $scope.ASSIGNMENT_ID = '76fd2c6f-9730-4e57-bf9a-4e294eaad3fc';
 
   }
 );
