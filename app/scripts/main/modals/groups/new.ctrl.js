f6f3bbfe0a725c297b71972af518d523d764610d
shegoyal <shegoyal@cisco.com>
2015-06-23 10:33:38 -0700

    Changes

--- app/scripts/main/modals/groups/new.ctrl.js
+++ app/scripts/main/modals/groups/new.ctrl.js
@@ -1,6 +1,7 @@
 angular.module('policyEngine').controller('NewGroupCtrl', function ($scope, PolicyActions) {
  $scope.types = {};
  $scope.itemList =[];
+ $scope.descriptions = false;
   $scope.group = {
     name: "New Group",
     pools: []
@@ -29,19 +30,30 @@ angular.module('policyEngine').controller('NewGroupCtrl', function ($scope, Poli
   ];
 
   $scope.changeData = function() {
-          console.log($scope.group.itemsuper);
-
           if($scope.group.itemsuper.name == "Device") {
               $scope.types = $scope.device;
           } else if($scope.group.itemsuper.name == "Location") {
               $scope.types = $scope.location;
           }
+
       };
 
    $scope.description =[''];
 
    $scope.addPosture =function() {
      $scope.description.push({});
+     $scope.descriptions =true;
+     $scope.options = true;
+     $scope.group.postureName = $scope.group.itemsuper;
+      $scope.group.valueName = $scope.group.item1;
    };
+   $scope.options= true;
+   $scope.change = function () {
+   if($scope.group.itemsuper != null && $scope.group.item1 != null)
+     $scope.options = false;
+     $scope.descriptions= true;
+     $scope.group.postureName = $scope.group.itemsuper;
+     $scope.group.valueName = $scope.group.item1;
+   }
 
 });
