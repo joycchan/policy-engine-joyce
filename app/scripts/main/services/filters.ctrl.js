bdf4941efab36dc47ed674ac7705e51e42894d18
Steven Guichard <ssguichard@gmail.com>
2015-06-24 10:25:24 -0700

    DRY up filtering logic

--- app/scripts/main/services/filters.ctrl.js
+++ app/scripts/main/services/filters.ctrl.js
@@ -6,35 +6,32 @@ angular.module('policyEngine').controller('ServicesFilterCtrl',
     $scope.filteredServices = [];
 
     var filterServices = function (allServices) {
+      var filters = [{
+          param: 'category',
+          func: $scope.category
+        }, {
+          param: 'group',
+          func: $scope.providerGroup
+        }, {
+          param: 'ruleSet',
+          func: $scope.ruleSet
+        }];
 
-      var result = allServices.slice();
-
-      //refactor when categories are an object
-      if ($stateParams['category']) {
-        result = _.filter(result, function (service) {
-          return $scope.category(service).name === $stateParams['category'];
-        });
-      }
-
-      if ($stateParams['group']) {
-        result = _.filter(result, function (service) {
-          return $scope.providerGroup(service).name === $stateParams['group'];
-        });
-      }
-
-      if ($stateParams['ruleSet']) {
-        result = _.filter(result, function (service) {
-          return $scope.ruleSet(service).name === $stateParams['ruleSet'];
-        });
-      }
-
+      var result = allServices;
+      filters.forEach(function(filter) {
+        if ($stateParams[filter.param]) {
+          result = _.filter(result, function(service) {
+            return filter.func(service).name === $stateParams[filter.param];
+          });
+        }
+      });
       return result;
     };
 
     $scope.$watchGroup(['$routeChangeSuccess', function () {
       return PolicyStore.Services.all();
     }], function () {
-      $scope.filteredServices = filterServices(PolicyStore.Services.all());
+      $scope.filteredServices = filterServices(PolicyStore.Services.all().slice());
       //Store a hash of which filters are in use so that service.html can display "Reset" links appropriately
       _.each(['category', 'group', 'ruleSet'], function (type) {
         $scope.filtered[type] = !!$stateParams[type];
