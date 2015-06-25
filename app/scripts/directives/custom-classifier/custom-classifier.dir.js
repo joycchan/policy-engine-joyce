a58c3a1ffdc64bda98c909075f78aeb1560eb572
Thuongvu Ho <thuongvuho@gmail.com>
2015-06-25 16:17:20 -0700

    remove: instances in which ClassifierCategories service was used

--- app/scripts/directives/custom-classifier/custom-classifier.dir.js
+++ app/scripts/directives/custom-classifier/custom-classifier.dir.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('policyEngine')
-  .directive('customClassifier', function (PolicyActions, PolicyStore, ClassifierCategories) {
+  .directive('customClassifier', function (PolicyActions, PolicyStore) {
     return {
       templateUrl: 'scripts/directives/custom-classifier/custom-classifier.html',
       controller: function ($scope) {
@@ -13,12 +13,6 @@ angular.module('policyEngine')
           {name: 'IP', value: 'IP'},
           {name: 'HTTP', value: 'HTTP'}
         ];
-
-        // this function creates an array of objects to ng-repeat over
-        // each object's "name" and "value" key are assigned the value of the category's name
-        $scope.categories = _.map(ClassifierCategories, function(category) {
-          return {"name": category, "value": category};
-        });
 
       },
       scope: {
