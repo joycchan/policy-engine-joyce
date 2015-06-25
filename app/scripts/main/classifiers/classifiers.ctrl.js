ba79bdf15816cee75150dc3bfc33b24918fa51c4
Thuongvu Ho <thuongvuho@gmail.com>
2015-06-25 15:04:27 -0700

    add: remove classifier categories so that the classifiers list is a single list instead of grouped

--- app/scripts/main/classifiers/classifiers.ctrl.js
+++ app/scripts/main/classifiers/classifiers.ctrl.js
@@ -1,36 +1,15 @@
 'use strict';
 
 angular.module('policyEngine').controller('ClassifiersCtrl',
-  function($scope, PolicyStore, ClassifierCategories) {
+  function($scope, PolicyStore) {
 
     $scope.search = {'name': ''};
 
     $scope.selected = {
-      categoryName: '',
-      classifiersList: [],
       classifier: null
     };
 
     $scope.classifiersList = PolicyStore.Classifiers.all.bind(PolicyStore.Classifiers);
-
-    $scope.categoryNames = ClassifierCategories;
-
-    $scope.classifierCategories = function() {
-      return _.groupBy($scope.classifiersList(), function(classifier) {
-        return classifier.category;
-      });
-    };
-
-    $scope.selectCategory = function(categoryName) {
-      $scope.selected.categoryName = categoryName;
-      $scope.selected.classifiersList = $scope.classifierCategories()[categoryName];
-      // reset selected classifier
-      $scope.selected.classifier = null;
-    };
-
-    $scope.isCategorySelected = function(category) {
-      return $scope.selected.categoryName === category;
-    };
 
     $scope.selectClassifier = function(classifier) {
       $scope.selected.classifier = classifier;
