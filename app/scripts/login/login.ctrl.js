e41f5992ef6c8c1c75c13a87f32d80fbee450bd0
Steven Guichard <ssguichard@gmail.com>
2015-06-26 12:06:28 -0700

    Added login content/styling

--- app/scripts/login/login.ctrl.js
+++ app/scripts/login/login.ctrl.js
@@ -1,7 +1,19 @@
 'use strict';
 
 angular.module('policyEngine').controller('LoginCtrl',
-  function ($scope) {
+  function ($scope, $state) {
 
+    $scope.focused = {
+      username: false,
+      password: false
+    };
+
+    $scope.shiftFocus = function(target) {
+      $scope.focused[target] = !$scope.focused[target];
+    };
+
+    $scope.login = function() {
+      $state.go('main.services.filters.cards');
+    };
   }
 );
