ab89ff1ce4a29a0b0b5dc36e5e2344edb86c7bd7
Thuongvu Ho <thuongvuho@gmail.com>
2015-06-26 09:20:53 -0700

    add: directive so that the custom classifier input field converts a string into a number for port value

---
+++ app/scripts/directives/integer-ng-model/integer-ng-model.dir.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('policyEngine')
+  .directive('integerNgModel', function() {
+    return {
+      require: 'ngModel',
+      link: function(scope, ele, attr, ctrl){
+        ctrl.$parsers.unshift(function(viewValue){
+          return parseInt(viewValue, 10);
+        });
+      }
+    };
+  });
