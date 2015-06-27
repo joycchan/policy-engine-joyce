4da3ed7b4818b209e9ca95f7485e8bd49fa7f144
Thuongvu Ho <thuongvuho@gmail.com>
2015-06-26 17:26:13 -0700

    chore: add comment on integer-ng-model

--- app/scripts/directives/integer-ng-model/integer-ng-model.dir.js
+++ app/scripts/directives/integer-ng-model/integer-ng-model.dir.js
@@ -5,6 +5,8 @@ angular.module('policyEngine')
     return {
       require: 'ngModel',
       link: function(scope, ele, attr, ctrl){
+        // used to watch the ng-model on an input element with type=text
+         // and convert the string into a number
         ctrl.$parsers.unshift(function(viewValue){
           return parseInt(viewValue, 10);
         });
