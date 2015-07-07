f6298c4130b185fb1c1ac7671ba70fcc65a96bff
Thuongvu Ho <thuongvuho@gmail.com>
2015-07-06 18:49:58 -0700

    add: global mock data for tests + chore: clarify comment

--- test/spec/directives/add-edit-rules/add-edit-rules.dir.js
+++ test/spec/directives/add-edit-rules/add-edit-rules.dir.js
@@ -3,43 +3,26 @@
 describe('Directive: add-edit-rules', function () {
   var element, scope, $compile, $httpBackend;
 
-  var mockRuleSet = {
-    "name": "SQL Access",
-    "custom": false,
-    "services": "None",
-    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras pellentesque massa ac urna dictum, non pretium nulla fermentum.",
-    "id": "1",
-    "_key": "lt-1",
-    "rules": [{
-        "actionIds": ["1"],
-        "classifierIds": ["1"],
-        "$$hashKey": "object:33"
-    }, {
-        "actionIds": ["5", "6"],
-        "classifierIds": ["1", "2"],
-        "$$hashKey": "object:34"
-    }]
-  };
-
-  var mockEmptyRuleSet = {
-    rules: []
-  };
-
   beforeEach(module('policyEngine'));
 
-  beforeEach(inject(function ($rootScope, _$compile_, _$httpBackend_) {
+  beforeEach(inject(function (_$httpBackend_, _$compile_, $rootScope) {
+    // To test a directive, the test runner loads the app, and it performs http
+    // GET requests in the .run method in app.js.  This boilerplate is only
+    // required because the tests will throw an error if we do not specify that
+    // we expected these http requests, even though we do not use the data from these calls.
     $httpBackend = _$httpBackend_;
-    $httpBackend.whenGET(/api.*/).respond(200); // Required for the .run method in app.js to run
+    $httpBackend.whenGET(/api.*/).respond(200); 
+    // ---------------------------------------------------------------
     $compile = _$compile_;
     scope = $rootScope.$new();
 
-    scope.ruleSet = mockRuleSet;
+    scope.ruleSet = GlobalMockData.ruleSet;
     element = '<add-edit-rules rule-set="ruleSet"></add-edit-rules>'
     element = $compile(element)(scope);
   }));
 
   it('should display "Edit" when rules exist', function() {
-    scope.ruleSet = mockRuleSet;
+    scope.ruleSet = GlobalMockData.ruleSet;
     scope.$digest();
     var text = element.find('.addEditRules').text().trim();
 
@@ -47,7 +30,7 @@ describe('Directive: add-edit-rules', function () {
   });
 
   it('should display "Add" when no rules exist', function() {
-    scope.ruleSet = mockEmptyRuleSet;
+    scope.ruleSet = GlobalMockData.emptyRuleSet;
     scope.$digest();
     var text = element.find('.addEditRules').text().trim();
 
