35f8bfcd7e31eb68d8a051a84b1cfba3f39af33a
Thuongvu Ho <thuongvuho@gmail.com>
2015-07-06 18:53:09 -0700

    remove: old mock data, chore: indent new mock data for tests

--- test/spec/globalMockData.js
+++ test/spec/globalMockData.js
@@ -1,30 +1,29 @@
 var GlobalMockData = {};
 
-beforeEach(function() {
-	GlobalMockData.ruleSet = {
-		"name": "SQL Access",
-		"custom": false,
-		"services": "None",
-		"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras pellentesque massa ac urna dictum, non pretium nulla fermentum.",
-		"id": "1",
-		"_key": "lt-1",
-		"rules": [{
-		    "actionIds": ["1"],
-		    "classifierIds": ["1"],
-		    "$$hashKey": "object:33"
-		}, {
-		    "actionIds": ["5", "6"],
-		    "classifierIds": ["1", "2"],
-		    "$$hashKey": "object:34"
-		}]
-	};
-	GlobalMockData.emptyRuleSet = {
-		"name": "SQL Access",
-		"custom": false,
-		"services": "None",
-		"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras pellentesque massa ac urna dictum, non pretium nulla fermentum.",
-		"id": "1",
-		"_key": "lt-1",
-		rules: []
-	};
-});
+GlobalMockData.ruleSet = {
+	"name": "SQL Access",
+	"custom": false,
+	"services": "None",
+	"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras pellentesque massa ac urna dictum, non pretium nulla fermentum.",
+	"id": "1",
+	"_key": "lt-1",
+	"rules": [{
+    "actionIds": ["1"],
+    "classifierIds": ["1"],
+    "$$hashKey": "object:33"
+	}, {
+    "actionIds": ["5", "6"],
+    "classifierIds": ["1", "2"],
+    "$$hashKey": "object:34"
+	}]
+};
+
+GlobalMockData.emptyRuleSet = {
+	"name": "SQL Access",
+	"custom": false,
+	"services": "None",
+	"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras pellentesque massa ac urna dictum, non pretium nulla fermentum.",
+	"id": "1",
+	"_key": "lt-1",
+	rules: []
+};
